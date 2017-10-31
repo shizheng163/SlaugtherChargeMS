c7b5bee9468f598219207b02226a1f0e6e2e3fc2
史正 <shizheng163@126.com>
2017-10-31 22:41:27 +0800

    no message

---
+++ SlaughterChargeMS/CommonModel/Models/Butcher/ButcherStatis.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonModel.Models
+{
+    /// <summary>
+    /// 屠宰加工信息显示节点
+    /// </summary>
+    public class ButcherStatis
+    {
+        /// <summary>
+        /// 前台索引 
+        /// </summary>
+        public int Index;
+
+        /// <summary>
+        /// 统计区间
+        /// </summary>
+        public string Range;
+        /// <summary>
+        /// 收入金额
+        /// </summary>
+        public string IncomeMoney;
+        /// <summary>
+        /// 支出金额
+        /// </summary>
+        public string PayMoney;
+        /// <summary>
+        /// 盈利金额
+        /// </summary>
+        public string ProfitMoney;
+    }
+}
